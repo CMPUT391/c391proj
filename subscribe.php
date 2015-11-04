e58ca479e81ed558628709c3f77e6b94824747ed
jaeyoon1 <jaeyoon1@ualberta.ca>
2015-11-04 14:04:46 -0700

    Add subscribe.php

---
+++ subscribe.php
@@ -0,0 +1,185 @@
+<html>
+    <body>
+        <?php
+        function get_bool_array($arr){
+            if(empty($arr)){
+                return array();
+            }
+            $last = $arr[count($arr)-1];
+            $outarr = array();
+            $outarr = array_fill(0,$last+1,false);
+            foreach($arr as $index){
+                $outarr[$index-1] = true;
+            }
+            
+            return $outarr;
+        }
+        
+        
+	    function add_subscription($conn,$sensor_id,$person_id){
+	        #check if value already exists or not
+	        #echo "adding subscription sid:$sensor_id pid:$person_id<br>";
+	        $sql = "SELECT * FROM subscriptions 
+	        WHERE sensor_id = $sensor_id AND person_id = $person_id";
+	        $stid = oci_parse($conn,$sql);
+	        $res = oci_execute($stid);
+	        $row = oci_fetch_array($stid,OCI_ASSOC);
+	        if(empty($row)){
+                #echo "added subscription sid:$sensor_id pid:$person_id<br>";
+                $sql = "INSERT INTO subscriptions (sensor_id, person_id) 
+                        VALUES ($sensor_id,$person_id)";
+                $stid = oci_parse($conn,$sql);
+                $res = oci_execute($stid);
+            } else {
+                #echo " value aleady exists <br>";
+            }
+            
+            if (!$res) {
+		        $err = oci_error($stid);
+		        echo htmlentities($err['message']);
+            } else { 
+                #echo 'Rows Extracted <br/>'; 
+            }
+            oci_free_statement($stid);
+	    }
+	    
+	    function remove_subscription($conn,$sensor_id,$person_id){
+	        #check if value already exists or not
+	        #echo "removing subscription sid:$sensor_id pid:$person_id<br>";
+	        $sql = "SELECT * FROM subscriptions 
+	        WHERE sensor_id = $sensor_id AND person_id = $person_id";
+	        $stid = oci_parse($conn,$sql);
+	        $res = oci_execute($stid);
+	        $row = oci_fetch_array($stid,OCI_ASSOC);
+	        if(empty($row)){
+	            #echo "value does not exist <br>";
+            } else {
+                #echo "removed subscription sid:$sensor_id pid:$person_id<br>";
+                $sql = "DELETE FROM subscriptions
+                WHERE sensor_id = $sensor_id AND person_id = $person_id";
+                $stid = oci_parse($conn,$sql);
+                $res = oci_execute($stid);
+            }
+            
+            if (!$res) {
+		        $err = oci_error($stid);
+		        echo htmlentities($err['message']);
+            } else { 
+                #echo 'Rows Extracted <br/>'; 
+            }
+            oci_free_statement($stid);
+	    }
+	    
+	    function get_sensors($conn){
+	        #echo 'getting all sensors<br>';
+	        $arr = array();
+	        $sql = "SELECT * FROM sensors";
+	        $stid = oci_parse($conn,$sql);
+	        $res = oci_execute($stid);
+	        
+	        if (!$res) {
+		        $err = oci_error($stid);
+		        echo htmlentities($err['message']);
+            } else { 
+                #echo 'Rows Extracted <br/>'; 
+            }
+	        
+	        
+	        while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+		        foreach ($row as $item) {
+		            #echo $item.'&nbsp;';
+		        }
+		        array_push($arr,$row);
+		        #echo '<br/>';
+	        }
+	        oci_free_statement($stid);
+	        return $arr;
+	    }
+	    
+	    function get_subscribed_sensors($conn,$person_id){
+	        #echo "getting all sensored subscribed by pid:$person_id<br>";
+	        $arr = array();
+	        $sql = "SELECT * FROM subscriptions
+	        WHERE person_id=$person_id";
+	        $stid = oci_parse($conn,$sql);
+	        $res = oci_execute($stid);
+	        
+	        if (!$res) {
+		        $err = oci_error($stid);
+		        echo htmlentities($err['message']);
+            } else { 
+                #echo 'Rows Extracted <br/>'; 
+            }
+	        
+	        while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+		        foreach ($row as $item) {
+		            #echo $item.'&nbsp;';
+		        }
+		        array_push($arr,$row);
+		        #echo '<br/>';
+	        }
+	        oci_free_statement($stid);
+	        return $arr;
+	    }
+	    
+	    function is_subscribed($conn,$sensor_id,$person_id){
+	        $sql = "SELECT * FROM subscriptions
+	        WHERE sensor_id=$sensor_id AND person_id=$person_id";
+	        $stid = oci_parse($conn,$sql);
+	        $res = oci_execute($stid);
+	        if (!$res) {
+		        $err = oci_error($stid);
+		        echo htmlentities($err['message']);
+            } else { 
+                #echo 'Rows Extracted <br/>'; 
+            }
+            if($row = oci_fetch_array($stid,OCI_ASSOC)){
+                return true;
+            } else {
+                return false;
+            }
+	    }
+
+
+	    include ("PHPconnectionDB.php");        
+	    //establish connection
+            $conn=connect();
+            echo "<form name='submit1' method='POST' action='subscribe.php'>";
+            $pid = 1; //grab it from jimmy's stuff
+           	               
+           	              
+            $rows = get_sensors($conn);
+            if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+                $submit = $_POST['sensors'];
+                $i = 0;
+                #echo $submit;
+                $checked = get_bool_array($submit);
+                foreach($rows as $row){
+                    #echo $checked[$i];
+                    if($checked[$i]){
+                        add_subscription($conn,$row['SENSOR_ID'],$pid);
+                        #echo 'true';
+                    } else {
+                        remove_subscription($conn,$row['SENSOR_ID'],$pid);
+                        #echo 'false';
+                    }
+                    $i++;
+                }
+            }
+    
+            
+            
+            foreach($rows as $row){
+                if(is_subscribed($conn,$row['SENSOR_ID'],$pid)){
+                    echo "<input type='checkbox' name='sensors[]' value='".$row['SENSOR_ID']."' checked>".$row['LOCATION'].'<br>';
+                } else {
+                    echo "<input type='checkbox' name='sensors[]' value='".$row['SENSOR_ID']."'>".$row['LOCATION'].'<br>';
+                }
+            }
+            oci_close($conn);
+            echo "<input type='submit' name='submit' value='submit'></form>";
+	
+	    ?>
+    </body>
+</html>
+
