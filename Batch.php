217242fe002307bd53f4d5db37372ab54e0235a4
jimmyho899 <jph@ualberta.ca>
2015-11-18 15:04:58 -0700

    Batch uploading finished.

--- Batch.php
+++ Batch.php
@@ -1,10 +1,12 @@
 <html>
     <body>    	
 		<?php
+		include("PHPconnectionDB.php");
 		// http://stackoverflow.com/questions/21332380/csv-upload-with-php-mysql
+		
 		if(isset($_POST['submit']) && $_FILES['batchToUpload']['size'] > 0) {
 			// csv file in format sensor_id, date (dd/mm/yyyy time), value
-			$csv_file = $_FILES['batchToUpload'];
+			$csv_file = $_FILES['batchToUpload']['tmp_name'];
 			$separator = ',';
 
 			// store information into the array import queries
@@ -17,13 +19,21 @@
 				$err = oci_error($stid); 
 				echo htmlentities($err['message']);
 			}
-			$new_id = $res + 1;
+			$row = oci_fetch_array($stid, OCI_ASSOC);
+			if (empty($row)) {
+				// no id exists, start at 1
+				$new_id = 1;
+			} else {
+				foreach ($row as $item) {
+					$new_id = $item + 1; // add 1 to max
+				}
+			}
 			oci_free_statement($stid);
 
 			if (($handle = fopen($csv_file, "r")) !== FALSE) {
-				while (($data = fgetcsv($handle, 1000, $separator, )) !== FALSE) {
+				while (($data = fgetcsv($handle, 1000, $separator)) !== FALSE) {
 					list($sensor_id, $date, $value) = $data;
-					$import_queries[] = 'INSERT INTO scalar_data VALUES (\''.$new_id.'\', \''.$sensor_id.'\', \''.$date.'\', \''.$value.'\')';
+					$import_queries[] = 'INSERT INTO scalar_data VALUES (\''.$new_id.'\', \''.$sensor_id.'\', to_date(\''.$date.'\', \'dd-mm-yyyy hh24:mi:ss\'), \''.$value.'\')';
 					$new_id++; // update for new unique id
 				}
 				fclose($handle);
@@ -32,6 +42,7 @@
 			// import queries now contains sql statements and we now have to execute them all (DONT KNOW IF WORKS)
 			if (sizeof($import_queries)) {
 				foreach($import_queries as &$queries) {
+					// echo $queries;
 					$stid = oci_parse($conn, $queries);
 					$res = oci_execute($stid);
 					if (!$res) {
@@ -46,9 +57,17 @@
 
 		} else {
 			echo "Nothing was submitted.";
+			?>
+			<a href ='UploadModule.html'>
+				<button>Retry</button>
+			</a>
+			<?php
 			return;
 		}
 		?>
-		<button><a href="UploadModule.html"> Go Back </a></button>
+		Successful Upload
+		<a href ='UploadModule.html'>
+			<button>Return</button>
+		</a>
     </body>
 </html>
